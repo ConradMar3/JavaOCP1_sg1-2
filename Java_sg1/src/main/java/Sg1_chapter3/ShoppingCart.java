3b468fddfeea64e0ddec9d7b0d7c02b3336ba5ed
ConradMare <conradmare890317@gmail.com>
2022-02-09 14:43:26 +0200

    2022/02/09
    Java.sg1 - Start

--- Java_sg1/src/main/java/Sg1_chapter3/ShoppingCart.java
+++ Java_sg1/src/main/java/Sg1_chapter3/ShoppingCart.java
@@ -2,17 +2,17 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Sg1_chapter2;
+package Sg1_chapter3;
 
 /**
  *
  * @author Conrad
  */
-public class SayHello {
+public class ShoppingCart {
+    // fields of the class
+    // methods
     public static void main(String[] args) {
+        // code goes here in the main block
         System.out.println("Hello World!");
     }
 }
-
-// javac SayHello.java to compile
-// java SayHello to run
