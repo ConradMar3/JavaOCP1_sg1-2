95fe34e6bc95471fe0c22d1de10b64319245e28f
ConradMare <conradmare890317@gmail.com>
2022-02-09 17:46:46 +0200

    2022/02/09
    _sg1 book.

---
+++ Java_sg1/src/main/java/Sg1_Chapter4/String.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Sg1_Chapter4;
+
+/**
+ *
+ * @author maria
+ */
+public class String {
+    public static void main(String[] args) {
+        String firstName = "Mary";
+        String lastName = "Hada";
+        String title = "LittleLambada";
+        
+        System.out.println(firstName + lastName + title);
+        
+    }
+    
+}
